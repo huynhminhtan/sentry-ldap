303692da7fafc52153e106feb3546335bb229476
huynhminhtan <huynhminhtan.vn@smilegate.sg>
2023-06-29 15:29:31 +0700

    chore: remove the domain part from username

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = [
 
 setup(
     name='sentry-ldap',
-    version='1.2.15',
+    version='1.2.16',
     author='',
     author_email='',
     url='https://github.com/huynhminhtan/sentry-ldap',
